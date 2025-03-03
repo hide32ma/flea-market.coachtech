28db2c215dc09af31c04f04575f7558dfa691811
hide32 <hide32.1219@gmail.com>
2025-03-03 10:23:00 +0900

    layout作成途中

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -2,14 +2,27 @@
 @extends('layouts.app')
 
 @section('css')
+<!-- このページで使用するcssを呼び出し -->
 <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
 @endsection
 
+
+@section('link')
+<!-- ヘッダーのリンクを記述する -->
+
+
+@endsection
+
+
+@section('content-title')
+<!-- h2タグを記述する -->
+
+
+@endsection
+
+
 @section('content')
-
-
-
-
+<!-- コンテンツを記述する -->
 
 
 @endsection
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>COACHTECH-FLEA-MARKET</title>
+    <title>flea-market</title>
 
     <!-- sanitize.css呼び出し -->
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
@@ -18,11 +18,24 @@
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner">
-            <h1 class="header__logo">aaa</h1>
+    <div class="app">
+        <header class="header">
+            <h1 class="header-heading">
+                <img src="{{ asset('img/logo.svg') }}" alt="">
+            </h1>
+
+            <!-- ページによっては使用する -->
+            @yield('link')
+
+        </header>
+
+        <div class="content-title">
+
+            <!-- h2タグ -->
+            @yield('content-title')
+
         </div>
-    </header>
+    </div>
 
 <main>
 
